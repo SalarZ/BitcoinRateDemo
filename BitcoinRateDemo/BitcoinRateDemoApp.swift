9c7e33006cdc0c02a3529322a22919c772714561
salarZ <salar.zarandi@gmail.com>
2026-03-07 07:45:31 +0100

    Initial Commit

---
+++ BitcoinRateDemo/BitcoinRateDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinRateDemoApp.swift
+//  BitcoinRateDemo
+//
+//  Created by Salar on 3/7/26.
+//
+
+import SwiftUI
+
+@main
+struct BitcoinRateDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BitcoinRateDemoTests/BitcoinRateDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinRateDemoTests.swift
+//  BitcoinRateDemoTests
+//
+//  Created by Salar on 3/7/26.
+//
+
+import Testing
+@testable import BitcoinRateDemo
+
+struct BitcoinRateDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
