8d804dc70fda0891b4bb86d7754a88cc5f7919c2
salarZ <salar.zarandi@gmail.com>
2026-03-07 19:42:02 +0100

    Replace mock use cases with a concrete implementation

--- BitcoinRateDemo/BitcoinRateDemoApp.swift
+++ BitcoinRateDemo/BitcoinRateDemoApp.swift
@@ -9,12 +9,26 @@ import SwiftUI
 
 @main
 struct BitcoinRateDemoApp: App {
+    private let getCryptoHistoryUseCase: CryptoPriceHistoryUseCase
+    private let getCryptoCurrentPriceUseCase: CryptoCurrentPriceUseCase
+
+    init() {
+        let networkClient = DefaultNetworkClient(
+            httpClient: URLSession.shared,
+            baseURL: AppConstants.API.baseURL,
+            requestAuthorizer: APIKeyAuthorizer(apiKey: AppConfiguration.coinGeckoAPIKey))
+
+        let repository = NetworkCryptoPriceRepository(networkClient: networkClient)
+        self.getCryptoHistoryUseCase = DefaultCryptoPriceHistoryUseCase(repository: repository)
+        self.getCryptoCurrentPriceUseCase = DefaultCryptoCurrentPriceUseCase(repository: repository)
+    }
+
     var body: some Scene {
         WindowGroup {
             NavigationView {
                 BitcoinHistoryView(
-                    bitcoinHistoryItemsViewModel: BitcoinHistoryItemsViewModel(getCryptoHistoryUseCase: MockCryptoHistoryUseCase()),
-                    currentPriceCardViewModel: CurrentPriceCardViewModel(getCryptoCurrentPriceUseCase: MockGetCryptoCurrentPriceUseCase(), refreshInterval: 5))
+                    bitcoinHistoryItemsViewModel: BitcoinHistoryItemsViewModel(getCryptoHistoryUseCase: getCryptoHistoryUseCase),
+                    currentPriceCardViewModel: CurrentPriceCardViewModel(getCryptoCurrentPriceUseCase: getCryptoCurrentPriceUseCase))
             }
         }
     }
