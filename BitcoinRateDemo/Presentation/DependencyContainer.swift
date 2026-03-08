c93a8c3f26d661cff7c39836452f0324184165dd
salarZ <salar.zarandi@gmail.com>
2026-03-08 07:26:56 +0100

    Added `DependencyContainer`

---
+++ BitcoinRateDemo/Presentation/DependencyContainer.swift
@@ -0,0 +1,41 @@
+//
+//  DependencyContainer.swift
+//  BitcoinRateDemo
+//
+//  Created by Salar on 3/8/26.
+//
+
+import Foundation
+import SwiftUI
+
+struct DependencyContainer {
+
+    let priceHistoryUseCase: CryptoPriceHistoryUseCase
+    let currentPriceUseCase: CryptoCurrentPriceUseCase
+    let priceDetailsUseCase: CryptoPriceDetailsUseCase
+
+    init() {
+        let networkClient = DefaultNetworkClient(
+            httpClient: URLSession.shared,
+            baseURL: AppConstants.API.baseURL,
+            requestAuthorizer: APIKeyAuthorizer(apiKey: AppConfiguration.coinGeckoAPIKey))
+
+        let networkRepo = NetworkCryptoPriceRepository(networkClient: networkClient)
+        priceHistoryUseCase = DefaultCryptoPriceHistoryUseCase(repository: networkRepo)
+        currentPriceUseCase = DefaultCryptoCurrentPriceUseCase(repository: networkRepo)
+        priceDetailsUseCase = DefaultCryptoPriceDetailsUseCase(repository: networkRepo)
+    }
+}
+
+// MARK: - Environment
+
+private struct DependencyContainerKey: @MainActor EnvironmentKey {
+    @MainActor static let defaultValue = DependencyContainer()
+}
+
+extension EnvironmentValues {
+    @MainActor var container: DependencyContainer {
+        get { self[DependencyContainerKey.self] }
+        set { self[DependencyContainerKey.self] = newValue }
+    }
+}
